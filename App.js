1274f1126812ccbdb8c789f0315bd5369cb41b1f
NiravCoderoceans <nirav.coderoceans@gmail.com>
2023-03-16 11:07:48 +0530

    init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Trade Analysis</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
